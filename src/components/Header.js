2ddd6e6c64ca061958034b3cdfafbeb29385411f
Nicole Baird <nbaird44@gmail.com>
2022-01-25 15:06:09 -0500

    ginal header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 function Header() {
     return (
         <div className="header">
-            <h2 className="header-name">Nicole Baird</h2>
-            <h3 className="header-detail">Software Engineer</h3>
-            <h3 className="header-detail">Atlanta, GA</h3>
-            <p className="header-description">COMBINING A PASSION FOR FINDING CREATIVE SOLUTIONS TO DEVELOPMENT CHALLENGES WITH A KEEN EYE FOR INVENTIVE DESIGN</p>
+            <div className="all-details">
+                <h2 className="header-name">Nicole Baird</h2>
+                <h3 className="header-detail">Software Engineer</h3>
+                <h3 className="header-detail">Atlanta, GA</h3>
+            </div>
         </div>
     )
 }
