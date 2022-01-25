e1460de8d9e41a37d8ab77c2585eec5a541a9ed5
Nicole Baird <nbaird44@gmail.com>
2022-01-25 13:53:15 -0500

    header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
 function Header() {
     return (
         <div className="header">
-            <h2>Nicole Baird</h2>
-            <h2>Software Engineer</h2>
-            <h2>Atlanta, GA</h2>
-            <p>COMBINING A PASSION FOR FINDING CREATIVE SOLUTIONS TO DEVELOPMENT CHALLENGES WITH A KEEN EYE FOR INVENTIVE DESIGN</p>
+            <h2 className="header-name">Nicole Baird</h2>
+            <h3 className="header-detail">Software Engineer</h3>
+            <h3 className="header-detail">Atlanta, GA</h3>
+            <p className="header-description">COMBINING A PASSION FOR FINDING CREATIVE SOLUTIONS TO DEVELOPMENT CHALLENGES WITH A KEEN EYE FOR INVENTIVE DESIGN</p>
         </div>
     )
-    
 }
 
 export default Header
