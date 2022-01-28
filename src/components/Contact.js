0eae53d4bfbb6685eb216de7d90f30e768f4f743
Nicole Baird <nbaird44@gmail.com>
2022-01-28 17:25:51 -0500

    hover state for first project complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ function Contact() {
     return (
         <div className="contact">
             <h2 className="contact-headline">Thanks for stopping by!</h2>
-            <p className="contact-details">If you have any questions or would like to chat, please contact me!</p>
+            <p className="contact-details">If you have any questions or would like to chat, please contact me by clicking your preferred contact method below!</p>
             <div className="contact-icons">
                 <a href="mailto:nbaird44@gmail.com"><img className="contact-icon" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643147894/Portfolio/gmail_p5md2a.png" alt="email" /></a>
                 <a href="tel:4042474828"><img className="contact-icon" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643147894/Portfolio/phone_v7xr9r.png" alt="phone" /></a>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -22,10 +22,18 @@ function Portfolio(props) {
     return (
         <div className="portfolio">
             <h2 className="portfolio-headline">Portfolio</h2>
+            <h3 className="about-details">Hover over any record for more details. Want to view the full project? Just click!</h3>
         <div className="record-container">
             <div className="record-card">
                 <img style={recordShown} onMouseEnter={changeBackground} onMouseLeave={changeBackgroundBack} className="record" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643145893/Portfolio/project-one_agzw2w.png" alt="project-one" />
-                <div style={cardShown}><h2>Atlanta BucketList</h2></div>
+                <div style={cardShown} className="project-card">
+                    <div className="project-details">
+                        <h2 className="project-headline">Atlanta BucketList</h2>
+                        <p className="project-detail">Visiting Atlanta? Or considering it? Check out the best restaurants, attractions and breweries.</p>
+                        <p className="project-detail-tech">Technologies</p>
+                        <p className="project-detail-ind-tech">HTML | CSS | JAVASCRIPT</p>
+                    </div>
+                </div>
             </div>
             <img className="record" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643145893/Portfolio/project-one_agzw2w.png" alt="project-one" />
             <img className="record" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643145893/Portfolio/project-one_agzw2w.png" alt="project-one" />
