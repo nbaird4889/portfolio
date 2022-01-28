8b091153bc16f6a4ad5b9ebaef6f28ed321e6a2e
Nicole Baird <nbaird44@gmail.com>
2022-01-28 17:03:34 -0500

    functionality for hover state in portfolio complete

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,20 +1,32 @@
 import {useState} from "react"
 
 function Portfolio(props) {
-    const [isShown, setIsShown] = useState({display: "none"})
+    const [cardShown, setCardShown] = useState({display: "none"}) 
+    const [recordShown, setRecordShown] = useState({display: "block"})
 
     function changeBackground(e) {
-        if (e.target.className === "record") {
-            e.target.remove();
-            e.target.style.background = "pink"
-        }
+       return (
+           setCardShown({display: "block"}),
+           setRecordShown({display: "none"})
+       )
+    }
+
+    function changeBackgroundBack(e) {
+        return (
+            setCardShown({display: "none"}),
+            setRecordShown({display: "block"})
+        )
+        
     }
 
     return (
         <div className="portfolio">
             <h2 className="portfolio-headline">Portfolio</h2>
         <div className="record-container">
-            <img className="record" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643145893/Portfolio/project-one_agzw2w.png" alt="project-one" />
+            <div className="record-card">
+                <img style={recordShown} onMouseEnter={changeBackground} onMouseLeave={changeBackgroundBack} className="record" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643145893/Portfolio/project-one_agzw2w.png" alt="project-one" />
+                <div style={cardShown}><h2>Atlanta BucketList</h2></div>
+            </div>
             <img className="record" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643145893/Portfolio/project-one_agzw2w.png" alt="project-one" />
             <img className="record" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643145893/Portfolio/project-one_agzw2w.png" alt="project-one" />
             <img className="record" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643145893/Portfolio/project-one_agzw2w.png" alt="project-one" />
