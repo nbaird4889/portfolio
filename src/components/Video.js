bf4792b1a23686d07cda60c8d382726a7bd85597
Nicole Baird <nbaird44@gmail.com>
2022-02-01 14:38:32 -0500

    mobile styling

--- src/components/Video.js
+++ src/components/Video.js
@@ -6,15 +6,15 @@ function Video() {
                     <source src='https://res.cloudinary.com/dhbumhhh1/video/upload/v1642990188/Portfolio/Untitled_xcf05o.mp4' type="video/mp4" />
             </video>
             <div className="entry-details">
-            <h1>Nicole Baird</h1>
-            <h2 id="title">Software Engineer</h2>
-            <p className="learn-more">Bringing creative solutions</p>
-            <p className="learn-more">to software challenges.</p>
-            <p className="learn-more" id="scroll">Click below </p>
-            <p className="learn-more">to see my solutions</p>
-            <Link to="/nicole-baird">
-            <img className="arrow" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1642990637/Portfolio/SeekPng.com_white-arrow-png_87169_zllqgg.png" alt="arrow" />
-            </Link>
+                <h1>Nicole Baird</h1>
+                <h2 id="title">Software Engineer</h2>
+                <p className="learn-more">Bringing creative solutions</p>
+                <p className="learn-more">to software challenges.</p>
+                <p className="learn-more" id="scroll">Click below </p>
+                <p className="learn-more">to see my solutions</p>
+                <Link to="/nicole-baird">
+                <img className="arrow" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1642990637/Portfolio/SeekPng.com_white-arrow-png_87169_zllqgg.png" alt="arrow" />
+                </Link>
             </div>
         </div>
     )
