c95cb1f472604a55eacb749177a0f57c47ffc513
Nicole Baird <nbaird44@gmail.com>
2022-02-09 14:40:27 -0500

    curved text in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,15 @@ function Header() {
     return (
         <div className="header">
             <div className="all-details">
-                <h2 className="header-name">Nicole Baird</h2>
-                <h3 className="header-detail">Software Engineer</h3>
-                <h3 className="header-detail">Atlanta, GA</h3>
+              <div id='warped'>
+                     <span class='w0'>N</span><span class='w1'>i</span><span class='w2'>c</span><span class='w3'>o</span><span class='w4'>l</span><span class='w5'>e</span><span class='w6'> </span><span class='w7'>B</span><span class='w8'>a</span><span class='w9'>i</span><span class='w10'>r</span><span class='w11'>d</span>
+              </div>
+            </div>
+
+            <div className="all-details2">
+            <div id='warped2'>
+                     <span class='w0'>A</span><span class='w1'>t</span><span class='w2'>l</span><span class='w3'>a</span><span class='w4'>n</span><span class='w5'>t</span><span class='w6'>a</span><span class='w7'>,</span><span class='w8'> </span><span class='w9'>G</span><span class='w10'>A</span>
+            </div>
             </div>
         </div>
     )
