695737a1caa2f0def1d018dc85062de630850737
Nicole Baird <nbaird44@gmail.com>
2022-02-14 16:34:30 -0500

    change to hover colors and update to resume link

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -103,7 +103,7 @@ function Portfolio(props) {
             <h3 className="about-details" id="port-text">Hover over any record for more details. Want to view the full project? Just click!</h3>
         <div className="record-container">
 
-                <a className="project-link" href="https://project-two-reco.herokuapp.com/" target="_blank">
+                <a className="project-link" href="https://project-two-reco.herokuapp.com/" target="_blank" rel="noreferrer">
                 <div className="record-card">
                 <img style={recordShownTwo} onMouseEnter={changeBackgroundTwo} onMouseLeave={changeBackgroundBackTwo} className="record" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643477309/Portfolio/recommendations-port_dcfuo5.png" alt="project-two" />
                 <div style={cardShownTwo} className="project-card" id="project-two">
@@ -117,49 +117,7 @@ function Portfolio(props) {
                 </div>
                 </a>
 
-                <a className="project-link" href="https://uga-football.netlify.app/" target="_blank">
-                <div className="record-card">
-                <img style={recordShownThree} onMouseEnter={changeBackgroundThree} onMouseLeave={changeBackgroundBackThree} className="record" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643737629/Portfolio/football-disc_w2hzic.png" alt="project-three" />
-                <div style={cardShownThree} className="project-card" id="project-three">
-                    <div className="project-two-details">
-                        <h2 className="project-two-headline">UGA Football Legends</h2>
-                        <p className="project-three-detail">Checkout the Dawg legends who paved the way for the 2021 National champions!</p>
-                        <p className="project-two-detail-tech">Built with:</p>
-                        <p className="project-two-detail-ind-tech">HTML | CSS | JAVASCRIPT | MONGODB | EXPRESS | NODE.JS | REACT</p>
-                    </div>
-                </div>
-                </div>
-                </a>
-
-                <a className="project-link" href="https://s-and-p-500.netlify.app/" target="_blank">
-                <div className="record-card">
-                <img style={recordShownFour} onMouseEnter={changeBackgroundFour} onMouseLeave={changeBackgroundBackFour} className="record" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643839131/Portfolio/stock-app_ce2yma.png" alt="project-four" />
-                <div style={cardShownFour} className="project-card">
-                    <div className="project-two-details">
-                        <h2 className="project-two-headline">S&P 500</h2>
-                        <p className="project-detail">By utilizing an API, the app lists all of the S&P 500 stocks and information on each one</p>
-                        <p className="project-two-detail-tech">Built with:</p>
-                        <p className="project-two-detail-ind-tech">HTML | CSS | JAVASCRIPT | AJAX | NODE.JS | REACT</p>
-                    </div>
-                </div>
-                </div>
-                </a>
-
-                <a className="project-link" href="https://my-sustainable-brands.netlify.app/" target="_blank">
-                <div className="record-card">
-                <img style={recordShownFive} onMouseEnter={changeBackgroundFive} onMouseLeave={changeBackgroundBackFive} className="record" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643922916/Portfolio/msb-icons_qvjcel.png" alt="project-five" />
-                <div style={cardShownFive} className="project-card" id="project-two">
-                    <div className="project-two-details">
-                        <h2 className="project-two-headline">My Sustainable Brands</h2>
-                        <p className="project-two-detail">This application makes it easy to shop with sustainable brands and recommend your favorite brands to others.</p>
-                        <p className="project-two-detail-tech">Built with:</p>
-                        <p className="project-two-detail-ind-tech">HTML | CSS | JAVASCRIPT | MONGODB | EXPRESS | NODE.JS | REACT</p>
-                    </div>
-                </div>
-                </div>
-                </a>
-
-            <a className="project-link" href="https://atlanta-bucketlist.netlify.app/" target="_blank">
+                <a className="project-link" href="https://atlanta-bucketlist.netlify.app/" target="_blank" rel="noreferrer">
                 <div className="record-card">
                     <img style={recordShownOne} onMouseEnter={changeBackgroundOne} onMouseLeave={changeBackgroundBackOne} className="record" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643145893/Portfolio/project-one_agzw2w.png" alt="project-one" />
                     <div style={cardShownOne} className="project-card" id="project-three">
@@ -172,6 +130,50 @@ function Portfolio(props) {
                     </div>
                 </div>
             </a>
+
+            <a className="project-link" href="https://my-sustainable-brands.netlify.app/" target="_blank" rel="noreferrer">
+                <div className="record-card">
+                <img style={recordShownFive} onMouseEnter={changeBackgroundFive} onMouseLeave={changeBackgroundBackFive} className="record" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643922916/Portfolio/msb-icons_qvjcel.png" alt="project-five" />
+                <div style={cardShownFive} className="project-card">
+                    <div className="project-two-details">
+                        <h2 className="project-two-headline">My Sustainable Brands</h2>
+                        <p className="project-two-detail">This application makes it easy to shop with sustainable brands and recommend your favorite brands to others.</p>
+                        <p className="project-two-detail-tech">Built with:</p>
+                        <p className="project-two-detail-ind-tech">HTML | CSS | JAVASCRIPT | MONGODB | EXPRESS | NODE.JS | REACT</p>
+                    </div>
+                </div>
+                </div>
+            </a>
+
+                <a className="project-link" href="https://uga-football.netlify.app/" target="_blank" rel="noreferrer">
+                <div className="record-card">
+                <img style={recordShownThree} onMouseEnter={changeBackgroundThree} onMouseLeave={changeBackgroundBackThree} className="record" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643737629/Portfolio/football-disc_w2hzic.png" alt="project-three" />
+                <div style={cardShownThree} className="project-card" id="project-two">
+                    <div className="project-two-details">
+                        <h2 className="project-two-headline">UGA Football Legends</h2>
+                        <p className="project-three-detail">Checkout the Dawg legends who paved the way for the 2021 National champions!</p>
+                        <p className="project-two-detail-tech">Built with:</p>
+                        <p className="project-two-detail-ind-tech">HTML | CSS | JAVASCRIPT | MONGODB | EXPRESS | NODE.JS | REACT</p>
+                    </div>
+                </div>
+                </div>
+                </a>
+
+                <a className="project-link" href="https://s-and-p-500.netlify.app/" target="_blank" rel="noreferrer">
+                <div className="record-card">
+                <img style={recordShownFour} onMouseEnter={changeBackgroundFour} onMouseLeave={changeBackgroundBackFour} className="record" src="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643839131/Portfolio/stock-app_ce2yma.png" alt="project-four" />
+                <div style={cardShownFour} className="project-card" id="project-three">
+                    <div className="project-two-details">
+                        <h2 className="project-two-headline">S&P 500</h2>
+                        <p className="project-detail">By utilizing an API, the app lists all of the S&P 500 stocks and information on each one</p>
+                        <p className="project-two-detail-tech">Built with:</p>
+                        <p className="project-two-detail-ind-tech">HTML | CSS | JAVASCRIPT | AJAX | NODE.JS | REACT</p>
+                    </div>
+                </div>
+                </div>
+                </a>
+
+            
         </div>
         </div>
     )
